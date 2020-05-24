9a9f5265d3bbceaffa450f9c17922414dfe8fe11
Minjun Kim <hi@minjun.kim>
2020-05-24 17:36:15 +0900

    테마 처리 관련 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Cookies from 'universal-cookie';
 
 import './_app.scss';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <meta charSet="utf-8" key="meta-charset" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" key="meta-viewport" />
-      <title>minjun.kim</title>
-      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css" />
-    </Head>
-    <Component {...pageProps} />
-  </>
-);
+type Props = AppProps & {
+  cookies: Cookies;
+};
+
+const App = ({ Component, pageProps }: Props) => {
+  useEffect(() => {
+    const cookies = new Cookies();
+    const theme = cookies.get('_theme');
+    if (typeof document !== 'undefined' && typeof theme !== 'undefined') {
+      document.documentElement.setAttribute('data-theme', theme);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" key="meta-charset" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" key="meta-viewport" />
+        <title>minjun.kim</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default App;

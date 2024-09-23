2ae401a570a3b99a02251142939ca1b34961cf1a
Minjun Kim <hi@minjun.kim>
2024-09-24 00:18:49 +0900

    파일 정리

--- src/components/BlogArticle/BlogArticle.tsx
+++ src/components/BlogArticle/BlogArticle.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import BlogHeader from '../BlogHeader';
-import WPContent from '../WPContent';
 
 import styles from './BlogArticle.module.scss';
 import BlogContent from '../BlogContent';
@@ -19,10 +18,6 @@ export const BlogArticle = ({ title, date, content }: Props) => {
         title={title}
         date={date}
       />
-      {/* <WPContent
-        value={content}
-        className={styles.content}
-      /> */}
       <BlogContent value={content} className={styles.content} />
     </article>
   );
--- src/components/WPContent/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './WPContent';
-export * from './WPContent';

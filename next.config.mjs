7a973db15a97d107690ad934f6163e8b85b20fd0
logan <hi@minjun.kim>
2024-09-24 03:54:27 +0000

    next.config 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/blog/2020/05/24/next-js-wp-graphql-static-blog",
+        destination: "/post/next-js-wp-graphql-static-blog",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
